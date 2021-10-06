9614cc9cdddd7a8321c30e644b4fb2118b090433
bakuchunin <bakuchunin@gmail.com>
2021-10-06 19:45:41 +0900

    修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
   describe '会員情報入力' do
     context '新規登録できるとき' do
       it '全ての項目を埋めれば登録できる' do
+        expect(@user).to be_valid
       end
     end
     context '新規登録できないとき' do
