12826b60d597083b83ad53287e68d60aa532e1fc
bakuchunin <bakuchunin@gmail.com>
2021-11-07 21:50:30 +0900

    Revert "Merge branch 'master' into 商品出品機能"
    This reverts commit 1077ccd46c2b79082c042d17d38044911174e3ce, reversing
    changes made to caafc3e6a3040fbd9e5d030daf59934f41327a03.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def items_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :delivery_charge_bearer_id, :prefecture_id,
+                                 :delivery_days_id, :price).merge(user_id: current_user.id)
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'test' }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Lorem.characters(min_alpha: 1, min_numric: 1, min_length: 6) }
+    password              { Faker::Lorem.characters(number: 6) }
     password_confirmation { password }
     last_name           { '山田' }
     last_name_kana      { 'ヤマダ' }
